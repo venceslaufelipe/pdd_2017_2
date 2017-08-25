e0508bf7c18d3030a1f4097493faefd8d2aa7357
Felipe Venceslau <31327811+venceslaufelipe@users.noreply.github.com>
2017-08-24 21:55:18 -0300

    macacoUpdateFelipe.py
    Eu adicionei um animal e uma fruta. Adicionei o parâmetro "saciar", que, dependendo da fruta, pode diminuir 1 ou 2(ou mais) da fome do animal. Adicionei também os valores de saciar em cada fruta.

--- s02e01_macaco/macaco.py
+++ s02e01_macaco/macaco.py
@@ -1,7 +1,8 @@
 class Fruta:
-    def __init__(self, nome = "abacate", cor = "verde"):
+    def __init__(self, nome = "abacate", cor = "verde", saciar = 1):
         self.cor = cor
         self.nome = nome
+        self.saciar = saciar
         self.existe = True
     def __str__(self):
         return self.nome
@@ -21,9 +22,12 @@ class Animal:
             if ((self.preferencia == fruta.nome) \
                 or (self.preferencia == fruta.cor)) \
                 and fruta.existe and (self.fome > 0):
-                self.fome -= 1
-                fruta.existe = False
-                print(self.nome, "comeu", fruta.nome)
+                    if fruta.saciar > 1:
+                        self.fome -= fruta.saciar
+                    else:
+                        self.fome -= 1
+                    fruta.existe = False
+                    print(self.nome, "comeu", fruta.nome)
 
 class Zoologico:
     def alimentar(self, frutas, animais):
@@ -34,10 +38,10 @@ class Zoologico:
                 del frutas[i]
 
 
-frutas = [Fruta("banana", "amarela"), Fruta("uva", "roxa"), Fruta("laranja", "amarela"),\
-          Fruta ("uva", "verde")]
+frutas = [Fruta("banana", "amarela", 1), Fruta("uva", "roxa", 1), Fruta("laranja", "amarela", 1),\
+          Fruta ("uva", "verde", 1), Fruta("tangerina", "laranja", 2)]
 frutas[0].nome = 'Banana'
-animais = [Animal("macaco", 1, "amarela"), Animal("xiwahwah", 1, "uva")]
+animais = [Animal("macaco", 5, "amarela"), Animal("xiwahwah", 7, "uva"), Animal("leão", 8, "tangerina")]
 zoo = Zoologico()
 zoo.alimentar(frutas, animais)
 animais[1].esperar()
